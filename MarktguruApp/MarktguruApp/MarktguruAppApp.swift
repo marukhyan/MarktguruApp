64b9107a5336cc89b928aba11a0ab1c72903ec20
marukhyan <59002642+marukhyan@users.noreply.github.com>
2025-05-15 19:56:55 +0400

    Add files via upload
    Created new dummy project

---
+++ MarktguruApp/MarktguruApp/MarktguruAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarktguruAppApp.swift
+//  MarktguruApp
+//
+//  Created by Davit Marukhyan on 15.05.25.
+//
+
+import SwiftUI
+
+@main
+struct MarktguruAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
